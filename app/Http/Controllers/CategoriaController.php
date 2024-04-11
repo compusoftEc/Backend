a91a2d0f2466a1c20133e38a43b3094c8ab6cc3e
Lseb1103 <66772757+Lseb1103@users.noreply.github.com>
2024-04-11 13:42:20 -0500

    Correcion Categoria Collection

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriaCollection;
 use App\Models\Categoria;
-use App\http\Resources\CategoriaCollection;
+
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
